235ed66d6d5ac07f1e049e3808d778a3dfbe1e65
Vasyl Larin <larinvasyl@protonmail.com>
2022-10-03 12:11:46 +0600

    Refactoring

--- loginApp/LoginViewController.swift
+++ loginApp/LoginViewController.swift
@@ -11,23 +11,29 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var usernameTF: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func forgotUsernamePassword(_ sender: Any) {
+        showAlert(withTitle: "Oooops!", withMessage: "Username: User \nPassword: Pass")
+        
+    }
+    
+    @IBAction func loginButtonDidTapped() {
+        guard let userName = usernameTF.text, let password = passwordTF.text else { return }
+        if userName != "User" || password != "Pass" {
+            showAlert(withTitle: "Incorrect login or password", withMessage: "Please make sure that you enter")
+            passwordTF.text = ""
+        }
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeScreenViewController else { return }
         welcomeVC.username = usernameTF.text
     }
-    
-    
-    @IBAction func forgotUsernamePassword(_ sender: Any) {
-        showAlert(withTitle: "Oooops!", withMessage: "Username: User \nPassword: Pass")
-      
-    }
-    
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         usernameTF.text = ""
@@ -41,16 +47,6 @@ class LoginViewController: UIViewController {
         }
     }
     
-    
-
-    @IBAction func loginButtonDidTapped() {
-        guard let userName = usernameTF.text, let password = passwordTF.text else { return }
-        if userName != "User" || password != "Pass" {
-            showAlert(withTitle: "Incorrect login or password", withMessage: "Please make sure that you enter")
-            passwordTF.text = ""
-        }
-        
-    }
 }
 
 extension LoginViewController {
